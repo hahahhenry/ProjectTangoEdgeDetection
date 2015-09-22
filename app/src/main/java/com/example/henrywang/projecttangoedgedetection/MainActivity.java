1b14721febd99a7b723c7ff5cc64cefcadabdd3e
hahahhenry <hmw521@gmail.com>
2015-09-22 11:40:24 -0400

    Tango
    tango

--- app/src/main/java/com/example/henrywang/projecttangoedgedetection/MainActivity.java
+++ app/src/main/java/com/example/henrywang/projecttangoedgedetection/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.atap.tangoservice.Tango;
+
 
 public class MainActivity extends ActionBarActivity {
+    Tango mTango;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTango = new Tango(this);
     }
 
     @Override
