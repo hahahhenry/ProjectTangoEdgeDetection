31e913a9691b67aabab6751cb6f039d9b1a21862
hahahhenry <hmw521@gmail.com>
2015-10-15 11:53:27 -0400

    UI Changes

---
+++ app/src/main/java/com/example/henrywang/projecttangoedgedetection/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.example.henrywang.projecttangoedgedetection;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by henrywang on 10/15/15.
+ */
+public class HelpActivity extends ActionBarActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+    }
+}
--- app/src/main/java/com/example/henrywang/projecttangoedgedetection/MainActivity.java
+++ app/src/main/java/com/example/henrywang/projecttangoedgedetection/MainActivity.java
@@ -1,21 +1,49 @@
 package com.example.henrywang.projecttangoedgedetection;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.atap.tangoservice.Tango;
 
 
 public class MainActivity extends ActionBarActivity {
     //Tango mTango;
+    private Button bStart;
+    private Button bHelp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity);
         //mTango = new Tango(this);
-    }
+        bStart = (Button) findViewById(R.id.buttonStart);
+        bHelp = (Button) findViewById(R.id.buttonHelp);
+        bStart.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
 
+                    }
+                }
+        );
+
+        bHelp.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+
+                    }
+                }
+        );
+
+    }
+    /*Opens Help Menu*/
+    public void openHelp(View view){
+        //TODO:
+        Intent intent = new Intent(this, HelpActivity.class);
+    }
     public void makeFullScreen(){
 
     }
